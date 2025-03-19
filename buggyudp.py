2098ad5e9274e5333d90fa57e746969a11c11b9e
Adam Broderick <adambroderick148@gmail.com>
2025-03-19 12:23:20 +0000

    Added multithreading to 1st system to allow for multiple sockets on different ports

--- buggyudp.py
+++ buggyudp.py
@@ -1,6 +1,7 @@
 from socket import *
 import socket
 import random
+import threading
 
 
 def movement():
@@ -47,5 +48,52 @@ def movement():
         sock.close()
         print("Server closed.")
 
+def soil_sample():
+    host = socket.gethostname()
+    port = 5000
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((host, port))
+
+    print("UDP Server started on port", port)
+
+    try:
+        while True:
+            rand = random.randint(0,10)
+            data, addr = sock.recvfrom(1024)
+            message = data.decode().strip()
+            print(f"Received from {addr}: {message}")
+
+            if message.lower() == 'q':
+                print("Quit command received. Shutting down.")
+                response = "Server is shutting down."
+                sock.sendto(response.encode(), addr)
+                break
+
+            response = ""
+            match message:
+                case 'soil':
+                    response = "Sampling Soil"
+                case 'air':
+                    response = "Sampling Air"
+                case _:
+                    response = "Invalid Sample Command"
+                    
+            if rand < 4:
+                print("Simulating Lost Packet")
+                continue
+            if response:
+                sock.sendto(response.encode(), addr)
+    finally:
+        sock.close()
+        print("Server closed.")
+
 if __name__ == '__main__':
-    movement()
+    t1 = threading.Thread(target=movement)
+    t2 = threading.Thread(target=soil_sample)
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
--- controludp.py
+++ controludp.py
@@ -31,5 +31,43 @@ def movement():
     finally:
         sock.close()
 
+def soil_sample():
+    host = socket.gethostname()
+    port = 5000
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_addr = (host, port)
+    sock.settimeout(1)
+    try:
+        while True:
+            try:
+                command = get_input()
+                sock.sendto(command.encode(), server_addr)
+                if command.lower() == 'q':
+                    data, addr = sock.recvfrom(1024)
+                    print("Response:", data.decode())
+                    break
+                data, addr = sock.recvfrom(1024)
+                print("Response:", data.decode())
+            except timeout:
+                print("Request Timed Out")
+                continue
+    finally:
+        sock.close()
 if __name__ == '__main__':
-    movement()
+    while True:
+        print("Please Choose a function")
+        print("1: movement")
+        print("2: soil sampling")
+        print("exit: Exit the program")
+        choice = input("->")
+        match choice:
+            case '1':
+                movement()
+            case '2':
+                soil_sample()
+            case 'exit':
+                print("Program Terminating")
+                break
+            case _:
+                print("Invalid Input Command")
